02c7a5f5e181ded140cd46cdea1498a55762f554
amaify2 <tobe@trufin.io>
2025-12-05 11:20:38 +0000

    Initial setup for Keplr wallet

---
+++ src/wallets/keplr/actions/onboard.ts
@@ -0,0 +1,5 @@
+import { type Page } from "@playwright/test";
+
+export default async function onboard(page: Page) {
+    console.error("Not implemented");
+}
---
+++ src/wallets/keplr/keplr.ts
@@ -0,0 +1,113 @@
+import type { Page } from "@playwright/test";
+import { addAccount } from "./actions/add-account";
+import { getAccountAddress } from "./actions/get-account-address";
+import { lockWallet } from "./actions/lock";
+import onboard from "./actions/onboard";
+import { renameAccount } from "./actions/rename-account";
+import { switchAccount } from "./actions/switch-account";
+import { switchNetwork } from "./actions/switch-network";
+import { unlock } from "./actions/unlock";
+
+export class Keplr {
+    page: Page;
+
+    constructor(page: Page) {
+        this.page = page;
+    }
+
+    /**
+     * Onboards the wallet.
+     * This function onboards the wallet by entering the password and other required information.
+     * @param {OnboardingArgs} args - The arguments required for onboarding.
+     * @param args.mode - Create a new wallet or import via private key / mnemonic.
+     * @param args.password - The password for the wallet.
+     * @param args.secretRecoveryPhrase - The secret recovery phrase for the wallet when importing a wallet.
+     * @example
+     * const keplr = new Keplr(page);
+     * await keplr.onboard({ mode: "importPrivateKey", password: "password", privateKey: "private key" });
+     */
+    async onboard() {
+        await onboard(this.page);
+    }
+
+    /**
+     * Unlocks the wallet by entering the password.
+     * @example
+     * const keplr = new Keplr(page);
+     * await keplr.unlock();
+     */
+    async unlock() {
+        await unlock(this.page);
+    }
+
+    /**
+     * Locks the wallet by entering the password.
+     * This function locks the wallet by opening the settings page and then locking the wallet.
+     * @example
+     * const keplr = new Keplr(page);
+     * await keplr.lock();
+     */
+    async lock() {
+        await lockWallet(this.page);
+    }
+
+    /**
+     * Renames an account in the wallet.
+     * @param {Omit<RenameAccount, "page">} args - The arguments to rename the account.
+     * @param args.newAccountName - The new name of the account.
+     * @example
+     * const keplr = new Keplr(page);
+     * await keplr.renameAccount({ newAccountName: "New Account Name" });
+     */
+    async renameAccount() {
+        await renameAccount(this.page);
+    }
+
+    /**
+     * Switches the current network to the given network.
+     * @param {SwitchNetwork} networkName - The name of the network to switch to.
+     * @example
+     * const keplr = new Keplr(page);
+     * await keplr.switchNetwork("network name");
+     */
+    async switchNetwork(networkName: SwitchNetwork) {
+        await switchNetwork(this.page);
+    }
+
+    /**
+     * Switches the current account to the given account.
+     * @param {string} accountName - The name of the account to switch to.
+     * @example
+     * const keplr = new keplr(page);
+     * await keplr.switchAccount("Account 1");
+     */
+    async switchAccount(accountName: string) {
+        await switchAccount(this.page);
+    }
+
+    /**
+     * Retrieves the current account's address.
+     * @returns A promise that resolves with the current account's address as a string.
+     *
+     * @example
+     * const keplr = new Keplr(page);
+     * const address = await keplr.getAccountAddress();
+     */
+    async getAccountAddress() {
+        await getAccountAddress(this.page);
+    }
+
+    /**
+     * Adds an account to the wallet via a private key or mnemonic phrase.
+     * @param {{ accountName, ...args }: AddAccount} - The arguments to add the account.
+     * @param {string} args.accountName - The name of the account to add.
+     * @param {string} args.privateKey - The private key of the account to add, if the mode is "privateKey".
+     * @param {string[]} args.mnemonicPhrase - The mnemonic phrase of the account to add, if the mode is "mnemonic".
+     * @example
+     * const keplr = new Keplr(page);
+     * await keplr.addAccount(TBD);
+     */
+    async addAccount() {
+        await addAccount(this.page);
+    }
+}
