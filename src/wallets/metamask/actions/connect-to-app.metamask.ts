9b973f9b2bcabb6ee22278e684908059ecf50e7d
Ugwuanyi TobeChukwu <39131739+amaify@users.noreply.github.com>
2025-12-31 15:21:56 +0100

    [MetaMask] - Connecting to app, we should activate all onboarded accounts.

--- src/wallets/metamask/actions/connect-to-app.metamask.ts
+++ src/wallets/metamask/actions/connect-to-app.metamask.ts
@@ -6,6 +6,27 @@ import { switchAccount } from "./switch-account.metamask";
  * By default, the last account will be selected. If you want to select a specific account, pass `account` parameter.
  */
 export async function connectToApp(page: Page, account?: string) {
+    /**
+     * Grant permission to all accounts in the wallet.
+     * This is the first step so that we can easily switch to any desired account later.
+     * With this approach, we can switch accounts during a test session.
+     */
+    const editAccountsButton = page.getByTestId("edit");
+    await editAccountsButton.click();
+
+    const allAccounts = await page.locator("div[data-testid^='multichain-account-cell-entropy']").all();
+    for (const _account of allAccounts) {
+        const label = _account.locator("label > span > span");
+        const isLabelVisible = await label.isVisible().catch(() => false);
+        if (!isLabelVisible) {
+            await _account.click();
+        }
+    }
+
+    const connectMoreAccountsButton = page.getByTestId("connect-more-accounts-button");
+    await expect(connectMoreAccountsButton).toBeVisible();
+    await connectMoreAccountsButton.click();
+
     if (account) {
         await switchAccount({ page, accountName: account });
     }
